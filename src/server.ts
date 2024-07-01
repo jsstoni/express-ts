4e72b7fd938ce4f29864566c349e83819546012f
jsstoni <jsstoniha@gmail.com>
2024-06-30 21:07:27 -0400

    refactor: middleware to parse json and url-encoded

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express, { Application } from 'express';
 
 const app: Application = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 export { app };
