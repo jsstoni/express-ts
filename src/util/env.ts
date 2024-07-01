2ffda5c41353817f16293e3875f2ad95ba5933d9
jsstoni <jsstoniha@gmail.com>
2024-07-01 14:39:18 -0400

    feat: prisma client

--- src/util/env.ts
+++ src/util/env.ts
@@ -12,3 +12,5 @@ export const NODE_ENV = env
   .asEnum(validEnvironments) as Environment;
 
 export const PORT = env.get('PORT').required().asPortNumber();
+
+export const DATABASE_URL = env.get('DATABASE_URL').required().asString();
